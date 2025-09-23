a57abd2e5bfe324b8457ec49f4513c303ebd98d2
ChrishanAloka <164063707+ChrishanAloka@users.noreply.github.com>
2025-09-23 11:58:46 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -43,9 +43,9 @@ const Home = () => {
           {/* <Link to="/kitchen-login" className="btn btn-outline-dark btn-lg">
             Kitchen Login
           </Link> */}
-          <Link to="/admin-login" className="btn btn-outline-success btn-lg">
+          {/* <Link to="/admin-login" className="btn btn-outline-success btn-lg">
             Admin Login
-          </Link>
+          </Link> */}
         </div>
 
         <hr className="my-4" />
@@ -61,9 +61,9 @@ const Home = () => {
             {/* <Link to="/signup?role=kitchen" className="btn btn-dark btn-sm">
               Sign Up as Kitchen Staff
             </Link> */}
-            <Link to="/signup?role=admin" className="btn btn-success btn-sm">
+            {/* <Link to="/signup?role=admin" className="btn btn-success btn-sm">
               Sign Up as Admin
-            </Link>
+            </Link> */}
           </div>
         </div>
       </div>
